0e9e26b9db79f520bc4db67c9e719051c0d6f79c
Steinway Wu <steinwaywhw@gmail.com>
2012-12-13 02:52:39 -0500

    read/write/resize avaiable

--- fs.c
+++ fs.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <malloc.h>
 #include <string.h>
-
+#include <stdio.h>
 
 
 
@@ -49,6 +49,7 @@ fs_t* init_fs () {
 	sb->bitmap_ops.clear = bitmap_clear;
 	sb->bitmap_ops.clear_all = bitmap_clear_all;
 	sb->bitmap_ops.first_free = bitmap_first_free;
+	sb->bitmap_ops.clear_all (&sb->bitmap_ops);
 
 	sb->free_inodes = INODE_ARRAY_SIZE;
 	sb->free_blocks = OFFSET_LIMIT - OFFSET_FREE_BLOCK;
@@ -58,6 +59,7 @@ fs_t* init_fs () {
 	// setup root inode
 	index_node_t *root = &sb->inodes[INODE_ROOT_INDEX];
 	root->in_use = 1;
+	sb->free_inodes--;
 	strcpy (root->type, INODE_TYPE_DIR);
 	root->size = 0;
 
@@ -79,16 +81,18 @@ unsigned int bitmap_first_free (struct bitmap_ops_t *op) {
 	}
 	return -1;
 } 
+
 unsigned int bitmap_test (struct bitmap_ops_t *op, int free_block_number ) {
 	assert (free_block_number >= 0 && free_block_number + OFFSET_FREE_BLOCK < OFFSET_LIMIT);
 
 	char *p = (char *)op->start;
 	char data;
+	int bits = sizeof (char) * 8;
 
-	p += free_block_number / sizeof (char);
+	p += free_block_number / bits;
 	data = *p;
 
-	return (data >> (free_block_number % sizeof (char))) & 1;
+	return (data >> (free_block_number % bits)) & 1;
 }
 
 unsigned int bitmap_set (struct bitmap_ops_t *op, int free_block_number) {
@@ -96,13 +100,13 @@ unsigned int bitmap_set (struct bitmap_ops_t *op, int free_block_number) {
 
 	char *p = (char *)op->start;
 	char data;
-
-	p += free_block_number / sizeof (char);
+	int bits = sizeof (char) * 8;
+	p += free_block_number / bits;
 	data = *p;
 
-	int old = (data >> (free_block_number % sizeof (char))) & 1;	
+	int old = (data >> (free_block_number % bits)) & 1;	
 
-	*p |= 1 << (free_block_number % sizeof (char));
+	*p |= 1 << (free_block_number % bits);
 
 	return old;
 }
@@ -113,13 +117,13 @@ unsigned int bitmap_clear (struct bitmap_ops_t *op, int free_block_number) {
 
 	char *p = (char *)op->start;
 	char data;
-
-	p += free_block_number / sizeof (char);
+	int bits = sizeof (char) * 8;
+	p += free_block_number / bits;
 	data = *p;
 
-	int old = (data >> (free_block_number % sizeof (char))) & 1;	
+	int old = (data >> (free_block_number % bits)) & 1;	
 
-	*p &= ~(1 << (free_block_number % sizeof (char)));
+	*p &= ~(1 << (free_block_number % bits));
 	
 	return old;
 
@@ -166,8 +170,8 @@ int inode_create (super_block_t *sb, char *pathname, char *type) {
 }
 
 int fs_mk (fs_t *fs, char *pathname, char *type) {
-//	assert (fs != NULL);
-//	assert (pathname != NULL);
+	assert (fs != NULL);
+	assert (pathname != NULL);
 
 	super_block_t *sb = fs->super_block;
 
@@ -184,7 +188,7 @@ int fs_mk (fs_t *fs, char *pathname, char *type) {
 	if (parent < 0)
 		return -1;
 
-	// dir already exists
+	// child already exists
 	int child = inode_lookup_full (fs->super_block, pathname);
 	if (child >= 0)
 		return -1;
@@ -208,7 +212,7 @@ int fs_rm (fs_t *fs, char *pathname) {
 
 	// child not exists
 	int child = inode_lookup_full (fs->super_block, pathname);
-	if (child < -1)
+	if (child < 0)
 		return -1;
 
 	// child is non-empty dir
@@ -227,7 +231,7 @@ int inode_rm (super_block_t *sb, char *pathname) {
 	int parent = inode_lookup_parent (sb, pathname);
 	assert (parent >= 0);
 
-	// assume child exists, and it is an empty
+	// assume child exists, and it is an empty entry
 	int child = inode_lookup_full (sb, pathname);
 	assert (child >= 0);
 	assert (inode_isdir_isempty (sb, child) || inode_isreg (sb, child));
@@ -305,6 +309,7 @@ int inode_remove_dentry (super_block_t *sb, int inode, int dentry) {
 
 	while (status == sizeof (dir_entry_t)) {
 		fs_write (sb->fs, inode, offset - sizeof (dir_entry_t), &buffer, sizeof (dir_entry_t));
+		offset += sizeof (dir_entry_t);
 		status = fs_read (sb->fs, inode, offset, &buffer, sizeof (dir_entry_t));
 	}
 
@@ -578,6 +583,8 @@ int inode_write (super_block_t *sb, int index, int offset, void *buffer, int len
 	dst = loc_locate (location, offset);
 	to_copy = (offset / BLOCK_SIZE_IN_B + 1) * BLOCK_SIZE_IN_B - offset;
 	to_copy = to_copy > len ? len : to_copy;
+		printf ("Writing: %p, %d\n", dst, to_copy);
+
 	memcpy (dst, src, to_copy);
 
 	offset += to_copy;
@@ -587,6 +594,7 @@ int inode_write (super_block_t *sb, int index, int offset, void *buffer, int len
 	// second to second last
 	while (len > BLOCK_SIZE_IN_B) {
 		dst = loc_locate (location, offset);
+	printf ("Writing: %p, %d\n", dst, BLOCK_SIZE_IN_B);
 		memcpy (dst, src, BLOCK_SIZE_IN_B);
 
 		src += BLOCK_SIZE_IN_B;
@@ -596,6 +604,7 @@ int inode_write (super_block_t *sb, int index, int offset, void *buffer, int len
 
 	// last
 	dst = loc_locate (location, offset);
+	printf ("Writing: %p, %d\n", dst, len);
 	memcpy (dst, src, len);
 
 	return total_len;
@@ -661,7 +670,7 @@ int inode_resize (super_block_t *sb, int index, int size) {
 	}
 
 	// within the same block
-	if (size / BLOCK_SIZE_IN_B == inode->size / BLOCK_SIZE_IN_B) {
+	if ((size - 1) / BLOCK_SIZE_IN_B == (inode->size - 1) / BLOCK_SIZE_IN_B) {
 		inode->size = size;
 		return size;
 	}
@@ -685,39 +694,74 @@ int inode_shrink (super_block_t	*sb, int index, int size) {
 	assert (size < inode->size);
 
 	// within the same block
-	if (size / BLOCK_SIZE_IN_B == inode->size / BLOCK_SIZE_IN_B) {
+	if ((size - 1) / BLOCK_SIZE_IN_B == (inode->size - 1) / BLOCK_SIZE_IN_B) {
 		inode->size = size;
 		return size;
 	}
 
 	location_t *location = &inode->location;
-	
 	location_index_t location_index;
-	loc_index (location, size, &location_index);
 
-	// shrink level 1
-	if (location_index.level_2 == -1) {
-		int i;
-		for (i = location_index.level_1 + 1; i < LOC_NR_OF_DIRECT; i++) {
-			int block = fs_addr_to_free_block_number (sb->fs, location->direct[i]);
-			fs_free_block (sb->fs, block);
+	// current offset, init to the next block
+	int current = ((size - 1) / BLOCK_SIZE_IN_B + 1) * BLOCK_SIZE_IN_B;
+	if (size == 0)
+		current = 0;
+
+	while (current < LOC_LIMIT_DOUBLE_INDIRECT) {
+
+		loc_index (location, current, &location_index);
+
+		// not allocated, no need to shrink any more
+		if (loc_locate (location, current) == NULL)
+			break;
+
+		// 1 level shrink
+		if (current < LOC_LIMIT_DIRECT) {
+			inode_shrink_1_level (sb, &location->direct[location_index.level_1]);
+			current += BLOCK_SIZE_IN_B;
+			continue;
 		}
-	}
 
-	// shrink level 2
-	if (location_index.level_3 == -1) {
-		int i;
-		for (i = location_index.level_2 + 1; i < LOC_NR_OF_SINGLE_INDIRECT; i++) {
-			int block = fs_addr_to_free_block_number (sb->fs, location->single_indirect[i]);
-			fs_free_block (sb->fs, block);
+		// 2 level shrink
+		if (current < LOC_LIMIT_SINGLE_INDIRECT) {
+			void **p = (void **)location->single_indirect[location_index.level_1];
+			p += location_index.level_2;
+			inode_shrink_1_level (sb, p);
+
+			// check to free pointer block after freeing the 64th pointer
+			if (location_index.level_2 == LOC_POINTER_PER_BLOCK - 1) {
+				p = (void **)location->single_indirect[location_index.level_1];
+				if (*p == NULL)
+					inode_shrink_1_level (sb, &location->single_indirect[location_index.level_1]);
+			}
+
+			current += BLOCK_SIZE_IN_B;
+			continue;
 		}
-	}
 
-	// shrink level 3
-	int i;
-	for (i = location_index.level_3 + 1; i < LOC_LIMIT_DOUBLE_INDIRECT; i++) {
-		int block = fs_addr_to_free_block_number (sb->fs, location->double_indirect[i]);
-		fs_free_block (sb->fs, block);
+		// three level shrink
+		if (current < LOC_LIMIT_DOUBLE_INDIRECT) {
+			void **p = (void **)location->double_indirect[location_index.level_1];
+			p += location_index.level_2;
+
+			void **q = (void **)*p + location_index.level_3;
+			inode_shrink_1_level (sb, q);
+
+			// check to free level-2 pointer block
+			if (location_index.level_3 == LOC_POINTER_PER_BLOCK - 1) {
+				q = (void **)*p;
+				if (*q == NULL)
+					inode_shrink_1_level (sb, p);
+			}
+
+			// check to free level-1 pointer block
+			if (location_index.level_2 == LOC_POINTER_PER_BLOCK - 1) {
+				p = (void **)location->double_indirect[location_index.level_1];
+				if (*p == NULL)
+					inode_shrink_1_level (sb, &location->double_indirect[location_index.level_1]);
+			}
+
+		}
 	}
 
 	inode->size = size;
@@ -725,15 +769,26 @@ int inode_shrink (super_block_t	*sb, int index, int size) {
  	return size;
 } 
 
+int inode_shrink_1_level (super_block_t *sb, void **p) {
+	void *block = *p;
+	fs_free_block (sb->fs, fs_addr_to_free_block_number (sb->fs, block));
+	*p = NULL;
+
+	return 0;
+}
+
+
+// expend an inode to a given size
 int inode_expand (super_block_t *sb, int index, int size) {
 	assert (sb != NULL);
 	assert (index >=0 && index < INODE_ARRAY_SIZE);
-	assert (size < MAX_FILE_SIZE);
+	assert (size <= MAX_FILE_SIZE);
 
 	// no free blocks;
 	if (sb->free_blocks == 0)
 		return -1;
 
+	// get inode
 	index_node_t *inode = &sb->inodes[index];
 	assert (size > inode->size);
 
@@ -742,35 +797,42 @@ int inode_expand (super_block_t *sb, int index, int size) {
 		inode->location.direct[0] = fs_allocate_block (sb->fs);
 	}
 
-
-	// within the same block
-	if (size / BLOCK_SIZE_IN_B == inode->size / BLOCK_SIZE_IN_B) {
+	// the offset is within the same block, no need to expand
+	if ((size - 1) / BLOCK_SIZE_IN_B == (inode->size - 1) / BLOCK_SIZE_IN_B) {
 		inode->size = size;
 		return size;
 	}
 
 	location_t *location = &inode->location;
 
+	// allocation counter
 	int current = 0;
+
 	location_index_t location_index;
-	void *p;
+	void *p = NULL, *status = NULL;
 
 	// traverse and allocate all
 	while (current < size) {
 
+		// get current allocation index
 		loc_index (location, current, &location_index);
 
+		// one level allocation
 		if (current < LOC_LIMIT_DIRECT) {
 
 			// allocate data block
 			if (loc_locate (location, current) == NULL) {
-				location->direct[location_index.level_1] = fs_allocate_block (sb->fs);
+				status = fs_allocate_block (sb->fs);
+				assert (status != NULL);
+
+				location->direct[location_index.level_1] = status;
 			}
 			
 			current += BLOCK_SIZE_IN_B;
 			continue;
 		}
 
+		// two level allocation
 		if (current < LOC_LIMIT_SINGLE_INDIRECT) {
 
 			if (loc_locate (location, current) == NULL) {
@@ -778,21 +840,23 @@ int inode_expand (super_block_t *sb, int index, int size) {
 				// level 1 allocation
 				p = location->single_indirect[location_index.level_1];
 				if (p == NULL) {
+					status = fs_allocate_block (sb->fs);
+					assert (status != NULL);
 					location->single_indirect[location_index.level_1] = fs_allocate_block (sb->fs);
 				}
 
 				// data block allocation
 				p = location->single_indirect[location_index.level_1];
-				p += location_index.level_2;
+				p = (void *)((void**)p + location_index.level_2);
 				if (*(void **)p == NULL)
 					*(void **)p = fs_allocate_block (sb->fs);
-
 			}
 
 			current += BLOCK_SIZE_IN_B;
 			continue;
 		}
 
+		// three level allocation
 		if (current < LOC_LIMIT_DOUBLE_INDIRECT) {
 
 			if (loc_locate (location, current) == NULL) {
@@ -800,18 +864,20 @@ int inode_expand (super_block_t *sb, int index, int size) {
 				// level 1 allocation
 				p = location->double_indirect[location_index.level_1];
 				if (p == NULL) {
-					location->double_indirect[location_index.level_1] = fs_allocate_block (sb->fs);
+					p = fs_allocate_block (sb->fs);
+					assert (p != NULL);
+					location->double_indirect[location_index.level_1] = p;
 				}
 
 				// level 2 allocation
-				p = location->double_indirect[location_index.level_1];
-				p += location_index.level_2;
-				if (*(void **)p == NULL)
+				p = (void *)((void **)p + location_index.level_2);
+				if (*(void **)p == NULL) {
 					*(void **)p = fs_allocate_block (sb->fs);
+				}
 
 				// data block allocation
 				p = *(void **)p;
-				p += location_index.level_3;
+				p = (void *)((void **)p + location_index.level_3);
 				if (*(void **)p == NULL)
 					*(void **)p = fs_allocate_block (sb->fs);
 			}
@@ -828,65 +894,86 @@ int inode_expand (super_block_t *sb, int index, int size) {
 	return size;
 }
 
-
+// allocate a block, init with all zero
 void *fs_allocate_block (fs_t *fs) {
 	assert (fs != NULL);
 
+	// can't allocate
 	if (fs->super_block->free_blocks == 0)
 		return NULL;
 
+	// compute and get a free block
 	bitmap_ops_t *bitmap_ops = &fs->super_block->bitmap_ops;
 	int block = bitmap_ops->first_free (bitmap_ops);
+
+	// set bitmap
 	bitmap_ops->set (bitmap_ops, block);
+	// update counter
 	fs->super_block->free_blocks--;
+
+	// init with zero
+	memset (fs->device.locate (&fs->device, OFFSET_FREE_BLOCK + block), 0, BLOCK_SIZE_IN_B);
 
 	return fs->device.locate (&fs->device, OFFSET_FREE_BLOCK + block);
 }
 
+// free an allocated block, erase its content
 int fs_free_block (fs_t *fs, int free_block_number) {
 	assert (fs != NULL);
-	assert (free_block_number >= 0 && free_block_number < OFFSET_LIMIT);
+	assert (free_block_number >= 0 && free_block_number < OFFSET_LIMIT - OFFSET_FREE_BLOCK);
 
+	// clear bitmap
 	bitmap_ops_t *bitmap_ops = &fs->super_block->bitmap_ops;
 	bitmap_ops->clear (bitmap_ops, free_block_number);
 
+	// erase
 	memset (fs->device.locate (&fs->device, OFFSET_FREE_BLOCK + free_block_number), 0, BLOCK_SIZE_IN_B);
 
+	// update counter
 	fs->super_block->free_blocks++;
 
 	return 0;
 }
 
 int fs_addr_to_free_block_number (fs_t *fs, void *free_block_addr) {
-	assert (fs != NULL);
-	assert (free_block_addr != NULL);
-
 	return (free_block_addr - fs->super_block->blocks) / BLOCK_SIZE_IN_B;
 }
 
 void* fs_abs_block_number_to_addr (fs_t *fs, int abs_block_number) {
-	assert (fs != NULL);
-
 	return fs->device.locate (&fs->device, abs_block_number);
 }
 
 void* device_locate (device_t *device, int absolute_block_number) {
-	assert (device != NULL);
-	return device->start + absolute_block_number * BLOCK_SIZE_IN_B;
+	return (void *)((char *)device->start + absolute_block_number * BLOCK_SIZE_IN_B);
 }
 
+
+// locate the offset according to location object
 void* loc_locate (location_t *location, int offset) {
-	assert (location != NULL && offset >= 0 && offset < LOC_LIMIT_DOUBLE_INDIRECT);
+
+	assert (location != NULL);
+	assert (offset >= 0 && offset < LOC_LIMIT_DOUBLE_INDIRECT);
 
 	location_index_t index;
 
+	// get the index first
 	loc_index (location, offset, &index);
+	void *p = NULL;
 
-	void *p;
+	// one level indexing
+	if (offset < LOC_LIMIT_DIRECT) {
 
-	if (offset < LOC_LIMIT_DIRECT) 
-		return location->direct[index.level_1] + offset % BLOCK_SIZE_IN_B;
+		p = location->direct[index.level_1];
 
+		// not allocated yet
+		if (p == NULL)
+			return NULL;
+
+		// allocated, offset by byte
+		return (void *)((char *)p + (offset % BLOCK_SIZE_IN_B));
+	}
+		
+	// two level indexing
 	if (offset < LOC_LIMIT_SINGLE_INDIRECT) {
 
 		// not allocated yet
@@ -894,15 +981,17 @@ void* loc_locate (location_t *location, int offset) {
 		if (p == NULL)
 			return NULL;
 
-		// still not allocated yet
-		p += index.level_2;
+		// still not allocated yet, offset by 4 bytes
+		p = (void *)((void **)p + index.level_2);
 		p = *(void **)p;
 		if (p == NULL)
 			return NULL;
 
-		// allocated
-		return p + offset % BLOCK_SIZE_IN_B;
+		// allocated, offset by byte
+		return (void *)((char *)p + (offset % BLOCK_SIZE_IN_B));
 	}
+
+	// three level indexing
 
 	// not allocated yet
 	p = location->double_indirect[index.level_1];
@@ -910,32 +999,40 @@ void* loc_locate (location_t *location, int offset) {
 		return NULL;
 
 	// still not allocated yet
-	p += index.level_2;
+	p = (void *)((void **)p + index.level_2);
 	p = *(void **)p;
 	if (p == NULL)
 		return NULL;
 
 	// still not allocated yet
-	p = p + index.level_3;
+	p = (void *)((void **)p + index.level_3);
 	p = *(void **)p;
 	if (p == NULL)
 		return NULL;
 
-	return p + offset % BLOCK_SIZE_IN_B;
+	// allocated, offset by byte
+	return (void *)((char *)p + (offset % BLOCK_SIZE_IN_B));
 }
 
+
+// compute the expected index for indexing location object of a given offset
 int loc_index (location_t *location, int offset, location_index_t *index) {
-	assert (location != NULL && offset >= 0 && offset < LOC_LIMIT_DOUBLE_INDIRECT && index != NULL);
+	
+	assert (location != NULL);
+	assert (offset >= 0 && offset < LOC_LIMIT_DOUBLE_INDIRECT);
+	assert (index != NULL);
 
 	index->level_1 = -1;
 	index->level_2 = -1;
 	index->level_3 = -1;
 
+	// first level
 	if (offset < LOC_LIMIT_DIRECT) {
 		index->level_1 = offset / LOC_SIZE_1_LEVEL;
 		return 0;
 	}
 
+	// second level
 	if (offset < LOC_LIMIT_SINGLE_INDIRECT) {
 		offset -= LOC_LIMIT_DIRECT;
 		index->level_1 = offset / LOC_SIZE_2_LEVEL;
@@ -946,8 +1043,8 @@ int loc_index (location_t *location, int offset, location_index_t *index) {
 		return 0;
 	}
 
+	// third level
 	offset -= LOC_LIMIT_SINGLE_INDIRECT;
-
 	index->level_1 = offset / LOC_SIZE_3_LEVEL;
 
 	offset %= LOC_SIZE_3_LEVEL;
--- test_fs.c
+++ test_fs.c
@@ -1,17 +1,49 @@
 #include "fs.h"
+#include <string.h>
+#include <stdio.h>
+#define TEST_SIZE LOC_LIMIT_DOUBLE_INDIRECT
 
 int main () {
 
 	fs_t *fs = init_fs ();
+
+
+	// int i;
+	// for (i = 0; i < 1025; i++) {
+	// 	char buffer[20];
+	// 	sprintf (buffer, "/file%2d", i);
+	// 	int status = fs_mk (fs, buffer, "reg");
+	// 	if (status < 0)
+	// 		printf ("%d\n", i);
+	// }
+
+	// inode_shrink (fs->super_block, 0, 2048);
+
+	// for (i = 0; i < 1025; i++) {
+	// 	char buffer[20];
+	// 	sprintf (buffer, "/file%2d", i);
+	// 	int status = fs_rm (fs, buffer);
+	// 	if (status < 0)
+	// 		printf ("%d\n", i);
+	// }
+	
 	fs_mk (fs, "/hello", "reg");
 
 	int index = inode_lookup_full (fs->super_block, "/hello");
 
-	char buffer[4] = "HHH";
-	fs_write (fs, index, 0, buffer, 4);
-	fs_read (fs, index, 0, buffer, 4);
+	char buffer[TEST_SIZE];
+	int i = 0;
+	for (i = 0; i < TEST_SIZE; i++) {
+		buffer[i] = i % 10 + '0';
+	}
 
-	
+	fs_write (fs, index, 0, buffer, TEST_SIZE);
+	// memset (buffer, 0, TEST_SIZE);
+	// fs_read (fs, index, 0, buffer, TEST_SIZE);
+
+	// printf ("%d", strlen(buffer));
+
+	inode_shrink (fs->super_block, index, 0);
 
 	destroy_fs (fs);
 }
